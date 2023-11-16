e752c915d2a8218d2f866f1c30244aa6f12b0411
andrzejkrzyszycha <andrzej.krzyszycha@gmail.com>
2023-11-16 22:56:43 +0100

    clean entity

--- src/main/java/com/example/simplespring/model/Car.java
+++ src/main/java/com/example/simplespring/model/Car.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @Entity(name = "car")
 @AllArgsConstructor
 public class Car {
